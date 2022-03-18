98ec7d9a8a0dbe9b2ab58f368bf4cb51135b029b
Justin Conger <jhconger@GMAIL.COM>
2022-03-18 11:07:40 -0500

    completed exercise

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,9 +1,11 @@
 USE codeup_test_db;
 
-SELECT '1991' AS 'Albums released in';
+SELECT '1991' AS 'Albums released after';
 DELETE FROM albums WHERE release_date > 1991;
+
 SELECT 'Disco' AS 'Genre';
 DELETE FROM albums WHERE genre = 'disco';
+
 SELECT 'Whitney Houston' AS 'Albums by';
 DELETE FROM albums WHERE artist = 'Whitney Houston';
 
