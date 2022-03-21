c74b060a4d1643e0cd6dfd7f74465482e9432feb
Justin Conger <jhconger@GMAIL.COM>
2022-03-21 15:07:05 -0500

    finished this exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -16,3 +16,14 @@ SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
 SELECT * FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
 
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
+
+SELECT CONCAT(first_name, ' ', last_name) AS 'Full Name'FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e' ORDER BY last_name;
+
+SELECT * FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25;
+
+SELECT * FROM employees WHERE year(hire_date) >= 1990 AND year(hire_date) <= 1999 AND month(birth_date) = 12 AND day(birth_date) = 25;
+
+SELECT * FROM employees WHERE year(hire_date) >= 1990 AND year(hire_date) <= 1999 AND month(birth_date) = 12 AND day(birth_date) = 25 ORDER BY hire_date DESC, birth_date;
+
+SELECT DATEDIFF(NOW(), hire_date), first_name, last_name FROM employees WHERE year(hire_date) >= 1990 AND year(hire_date) <= 1999 AND month(birth_date) = 12 AND day(birth_date) = 25;
